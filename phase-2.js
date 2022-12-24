d859e070e353d10d50195225cc6c2c962f1477fa
kevin ocansey <kevinocansey11@gmail.com>
2022-12-24 18:38:05 +0000

    contains addNums10 and addManyNums10 function

--- phase-2.js
+++ phase-2.js
@@ -3,13 +3,23 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let result = []
+  for(let i = increment; i <= increment * 10; i+= increment){
+    let sum =  addNums(i)
+    result.push(sum)
+  }
+  return result
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
-
+  let result = []
+  for(let i = increment; i <= increment * 10; i+= increment){
+    let sum =  addManyNums(i)
+    result.push(sum)
+  }
+  return result
 }
 
 module.exports = [addNums10, addManyNums10];
