f22ec1d75baafc12a3b071d60e4b6f9feed8c122
kevin ocansey <kevinocansey11@gmail.com>
2022-12-24 19:34:16 +0000

    time benching phase 3: Timimg my code

--- phase-3.js
+++ phase-3.js
@@ -1,22 +1,31 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
-
-  // Your code here
-
+  // Copy your `addNums10` code here'
+  let result = []
+  for(let i = increment; i <= increment * 10; i+= increment){
+    let startTime = Date.now()
+      let sum =  addNums(i)
+    let endTime = Date.now()
+    console.log((endTime - startTime))
+    result.push(sum)
+  }
+  return result
 }
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
-// Then, add timing code
-
-  // Your code here
-
+  let result = []
+    for(let i = increment; i <= increment * 10; i+= increment){
+      let startTime = Date.now()
+        let sum =  addManyNums(i)
+      let endTime = Date.now()
+      console.log((endTime - startTime))
+      result.push(sum)
+    }
+  return result
 }
-
 
 n = 1000000
 console.log(`addNums(${n}): `);
